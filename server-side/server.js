5c171fa0871dc4beb7124329e0f2a9e7aafabe41
9802HEMENSAN <hmahilange9802@gmail.com>
2023-08-06 15:41:29 +0530

    port changed

--- server-side/server.js
+++ server-side/server.js
@@ -100,7 +100,7 @@ app.delete('/delete-all-users', async (req, res) => {
   });
   
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
